0b0ff4c21a55e347e47a4b43be9b01501d095e5c
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-01-29 04:54:20 -0800

    chore(create database):database - create political-party database -create political-office database [Finishes #163556578]

---
+++ dummy/dummyDatabase/politicalPartyDb.js
@@ -0,0 +1,10 @@
+export default [{
+  id: 1,
+  partyname: 'APC',
+  hqaddress: 'Lagos',
+  Phone: '1234567890',
+  email: 'apc@gmail.com',
+  regNumber: '1234567890poiuytrew',
+  imageUrl: 'https://localhost/desktop.img.jpg'
+}
+];
