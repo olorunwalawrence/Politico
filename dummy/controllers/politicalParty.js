69c78334ed095d5d0f2acb1ca19bd9c81c9c7afa
olorunwalawrence <olorunwalawrence5@gmail.com>
2019-01-30 01:15:31 -0800

    ft(create controller):controller
    - create political-party controller
    - admin is able to create party
    - edit party
    - delete party
    update party
    
    -create political office controller
    - admin is able to create office
    - get office
    
    [Finishes #163473286]
    -

---
+++ dummy/controllers/politicalParty.js
@@ -0,0 +1,159 @@
+/* eslint-disable require-jsdoc */
+import getSingleQuery from '../utils/utils';
+import politicalPartyDb from '../dummyDatabase/politicalPartyDb';
+
+export default class politicalParty {
+  /*
+    =========================================
+                Create political party
+    ==========================================
+    */
+  static createParty(req, res) {
+    const {
+ partyname, address, phone, email, regnumber, imgurl } = req.body;
+
+    if (!partyname || !address || !phone || !email || !regnumber || !imgurl) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'undefined field detected'
+      });
+    }
+    const data = {
+      id: politicalPartyDb.length + 1,
+      partyname,
+      address,
+      phone,
+      email,
+      regnumber,
+      imgurl
+    };
+
+    politicalPartyDb.push(data);
+    return res.status(200).json({
+      success: true,
+      message: 'politiccal party created successfully',
+      data
+    });
+  }
+
+  /*
+    =========================================
+    Get all political party
+    ==========================================
+    */
+
+  static getAllParty(req, res) {
+    return res.status(200).json({
+      success: true,
+      message: 'All political party retrieved successfully',
+      politicalPartyDb
+    });
+  }
+
+  /*
+    =========================================
+        Get a single political party
+    ==========================================
+    */
+
+  static getSingleParty(req, res) {
+    const id = parseInt(req.params.id, 10);
+
+    getSingleQuery(
+      politicalPartyDb,
+      id,
+      'party retrieved successfully',
+      'party does not exist',
+      res
+    );
+  }
+
+  /*
+    =========================================
+                Delete political party
+    ==========================================
+    */
+  static deleteSingleParty(req, res) {
+    const id = parseInt(req.params.id, 10);
+    politicalPartyDb.map((party, index) => {
+      if (party.id === id) {
+        politicalPartyDb.splice(index, 1);
+        return res.status(200).json({
+          success: true,
+          message: 'party deleted successfully'
+        });
+      }
+    });
+    return res.status(404).json({
+      success: false,
+      message: 'no such party found'
+    });
+  }
+
+  /*
+    =========================================
+                Update political party
+    ==========================================
+    */
+
+  static updateparty(req, res) {
+    const id = parseInt(req.params.id, 10);
+    let partyFound;
+    let partyIndex;
+    politicalPartyDb.map((party, index) => {
+      if (party.id === id) {
+        partyFound = party;
+        partyIndex = index;
+      }
+    });
+    if (!partyFound) {
+      return res.status(400).send({
+        status: 'updated',
+        success: 'false',
+        message: 'political party not found'
+      });
+    }
+    const updatedParty = {
+      id: partyFound.id,
+      partyname: req.body.partyname || partyFound.partyname,
+      address: req.body.address || partyFound.address,
+      phone: req.body.phone || partyFound.phone,
+      email: req.body.email || partyFound.email,
+      regnumber: req.body.regnumber || partyFound.regnumber,
+      imgurl: req.body.imgurl || partyFound.imgurl
+    };
+
+    politicalPartyDb.splice(partyIndex, 1, updatedParty);
+
+    return res.status(201).send({
+      success: 'true',
+      message: 'todo added successfully',
+      updatedParty
+    });
+  }
+
+  static DeleteParty(req, res) {
+    const id = parseInt(req.params.id, 10);
+    let partyFound;
+    let partyIndex;
+    politicalPartyDb.map((party, index) => {
+      if (party.id === id) {
+        partyFound = party;
+        partyIndex = index;
+      }
+    });
+    if (!partyFound) {
+      return res.status(400).send({
+        status: 'not found',
+        success: 'false',
+        message: 'political party not found'
+      });
+    }
+
+    politicalPartyDb.splice(partyIndex, 1);
+    return res.status(200).send({
+      success: 'true',
+      message: 'political party deleted successfuly'
+    });
+  }
+}
---
+++ dummy/controllers/politicaloffice.js
@@ -0,0 +1,66 @@
+/* eslint-disable require-jsdoc */
+import getSingleQuery from '../utils/utils';
+import politicalOfficeDb from '../dummyDatabase/politicalOfficeDb';
+
+
+export default class politicalOffice {
+  /*
+    =========================================
+    Create political office
+    ==========================================
+    */
+  static createOffice(req, res) {
+    if (!req.body.officename) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'office name is required'
+      });
+    } if (!req.body.officetype) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'hqaddress is required'
+      });
+    }
+    const { officename, officetype } = req.body;
+    const data = {
+      id: politicalOfficeDb.length + 1,
+      officename,
+      officetype
+    };
+
+    politicalOfficeDb.push(data);
+    return res.status(200).json({
+      success: true,
+      message: 'politiccal office created successfully',
+      data
+    });
+  }
+
+
+  /*
+    =========================================
+    Get political office
+    ==========================================
+    */
+
+  static getAllOffice(req, res) {
+    return res.status(200).json({
+      success: true,
+      message: 'All political office retrieved successfully',
+      politicalOfficeDb
+    });
+  }
+
+
+  /*
+    =========================================
+        Get a single political office
+    ==========================================
+    */
+
+  static getSingleOffice(req, res) {
+    const id = parseInt(req.params.id, 10);
+
+    getSingleQuery(politicalOfficeDb, id, 'political office retrieved successfully', 'party does not exist', res )
+  }
+}
